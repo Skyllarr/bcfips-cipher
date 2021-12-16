b86701d1f4c28f6a2cf51362bf27253fbb83b687
Diana Krepinska Vilkolakova <skyllarr@gmail.com>
2021-12-16 10:35:17 +0100

    sunjsse is not working in fips mode on oracle java

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ mvn exec:exec
 You should see the output:
 
 ```console
-Provider of SSL Context is: BCJSSE
-
-TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384 is among supported cipher suites: true
+Provider of SSL Context is: SunJSSE
+TLS_DHE_DSS_WITH_AES_128_CBC_SHA is among supported cipher suites: false
+Sun JSSE provider (FIPS mode, crypto provider SunPKCS11-testFipsPkcs)
 ```
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,20 @@
 import javax.net.ssl.SSLContext;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
 import java.security.Security;
 import java.util.Arrays;
 
-import org.bouncycastle.jsse.provider.BouncyCastleJsseProvider;
-import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
-
 public class Main {
 
-    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchProviderException, KeyManagementException {
-        Security.insertProviderAt(new BouncyCastleFipsProvider(),1);
-        Security.insertProviderAt(new BouncyCastleJsseProvider(("fips:BCFIPS")), 2);
+    public static void main(String[] args) throws NoSuchAlgorithmException, KeyManagementException {
 
-        SSLContext sslContext = SSLContext.getInstance("TLSV1.2", BouncyCastleJsseProvider.PROVIDER_NAME);
+        SSLContext sslContext = SSLContext.getInstance("TLSV1.2", Security.getProvider("SunJSSE"));
         sslContext.init(null, null, null);
 
         System.out.println("Provider of SSL Context is: " + sslContext.getProvider().getName());
-        System.out.println("TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384 is among supported cipher suites: " +
-                Arrays.asList(sslContext.getServerSocketFactory().getSupportedCipherSuites()).contains("TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"));
+//        System.out.println("TLS_DHE_DSS_WITH_AES_128_CBC_SHA is among supported cipher suites: " +
+//                Arrays.asList(sslContext.getServerSocketFactory().getSupportedCipherSuites()).contains("TLS_DHE_DSS_WITH_AES_128_CBC_SHA"));
+        System.out.println(sslContext.getProvider().getInfo());
+      System.out.println(Arrays.asList(sslContext.getServerSocketFactory().getSupportedCipherSuites()));
     }
 }
